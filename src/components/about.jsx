dcf5a62d502087fb2f7e7e8afcf0d286c61b124d
Aashiq Durga <aashiqdurga@gmail.com>
2020-07-26 19:02:22 +1200

    added components and sections

---
+++ src/components/about.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <section className="colorlib-about" data-section="about">
+                    <div className="colorlib-narrow-content">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                                    <div className="col-md-12">
+                                        <div className="about-desc">
+                                            <span className="heading-meta">About Me</span>
+                                            <h2 className="colorlib-heading">Who am I?</h2>
+                                            <p>I am a  Software Engineer by profession and music maker DJ by passion.</p>
+                                            <p>I have been DJ'n and making music for a few years having played at clubs, mobile gigs and having my mixes aired on national radio.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className="colorlib-about">
+                    <div className="colorlib-narrow-content">
+                        <div className="row">
+                            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                                <span className="heading-meta">What I do?</span>
+                                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+                            </div>
+                        </div>
+                        <div className="row row-pt-md">
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-1">
+                                    <span className="icon">
+                                        <i className="icon-bulb" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3>Event DJ </h3>
+                                        <p> Available for your live events be it private or public.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-3">
+                                    <span className="icon">
+                                        <i className="icon-phone3" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3>Production radio mixes</h3>
+                                        <p>Premixed jams to get the vibe going based on genre or theme.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-5">
+                                    <span className="icon">
+                                        <i className="icon-data" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3>Music Production</h3>
+                                        <p>Voice over recordings or creating custom beats for you next track recording with you playing you melodies.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
