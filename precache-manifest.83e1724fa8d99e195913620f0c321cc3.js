0d7cbe91ffeeffbf53f7326b5515cbebb16355c5
Aashiq Durga <aashiqdurga@gmail.com>
2020-07-27 13:35:31 +1200

    Updates

---
+++ precache-manifest.83e1724fa8d99e195913620f0c321cc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06c235341a5c0984d2eb4e8babc19d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b997d5df58b07df3870",
+    "url": "/static/css/main.3587f5ea.chunk.css"
+  },
+  {
+    "revision": "ffeab29d2ac69bfeaad1",
+    "url": "/static/js/2.cb5f6824.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cb5f6824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b997d5df58b07df3870",
+    "url": "/static/js/main.039e4b85.chunk.js"
+  },
+  {
+    "revision": "f90c3b8f696ecf1f7dde",
+    "url": "/static/js/runtime-main.16bd769d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
